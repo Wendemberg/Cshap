ddd684dde3c09d0a480bd18a9556f4fab313f314
Wendemberg <bergwer99@gmail.com>
2022-02-26 16:15:13 -0300

    Operadores Unarios

---
+++ Projetos/OperadoresUnarios/OperadoresUnarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OperadoresUnarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 5;
+
+            //a++; incremento
+           // b--; decremento
+
+            Console.WriteLine(++a); // atibuição de valores pre-fixado
+            Console.WriteLine(--b);
+        }
+    }
+}
