7c120e62908ba03da9e36cc34d1dbddc92107972
Wendemberg <bergwer99@gmail.com>
2022-02-26 16:30:31 -0300

    Operadores de omparação

---
+++ Projetos/OperadorComparacao/OperadorComparacao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperadorComparacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 2;
+            var b = 3;
+            var iguais = a == b;
+            var diferentes = a != b;
+            var maior = a > b;
+            var menor = a < b;
+            var maiorOuIgual = a >= b;
+            var menorOuIgual = a <= b;
+
+            Console.WriteLine(iguais);
+            Console.WriteLine(diferentes);
+            Console.WriteLine(maior);
+            Console.WriteLine(menor);
+            Console.WriteLine(maiorOuIgual);
+            Console.WriteLine(menorOuIgual);
+        }
+    }
+}
