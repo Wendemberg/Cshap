acbe21bd2ea2cb0eef9f698088ee184518f5eb0e
Wendemberg <bergwer99@gmail.com>
2022-02-26 17:47:35 -0300

    Operador Ternário

---
+++ Projetos/OperadorTernario/OperadorTernario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperadorTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mensagem = "";
+            var idade = 15;
+
+            /*  if (idade >= 18)
+                    mensagem = "Maior de idade";
+                else
+                    mensagem = "Menor de idade";
+                Console.WriteLine(mensagem); */
+
+            //CONDIÇÃO ? VALOR SE VERDADEIRO : VALOR SE FALSO 
+
+            mensagem = idade >= 18 ? "Maior de idade" : "Menor de idade";
+            Console.WriteLine(mensagem);
+        }
+    }
+}
