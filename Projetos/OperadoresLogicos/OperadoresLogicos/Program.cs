665ed2efb8cfce7c2704c61ece16c8b62e732020
Wendemberg <bergwer99@gmail.com>
2022-02-26 16:46:23 -0300

    Operadores Logicos

---
+++ Projetos/OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ativo = false;
+            var a = 3;
+            var b = 5;
+           
+            Console.WriteLine(!ativo); //o sinal de exclamação indica a negação do valor contido na variável
+            Console.WriteLine(!(a > b));
+            Console.WriteLine(ativo == true && a > b);
+            Console.WriteLine(ativo == false && a < b);
+            Console.WriteLine(ativo == true || a > b);
+        }
+    }
+}
