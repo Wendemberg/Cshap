78ba901b3d51dd14b86a9ca29f7bc7f987f1a178
Wendemberg <bergwer99@gmail.com>
2022-02-27 00:59:22 -0300

    Estruturas de repetição

---
+++ Projetos/EsstrutuRepeticaoFor/EsstrutuRepeticaoFor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EstruturaRepeticaoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeros = new[] { 1, 2, 3, 4 };
+          //for(var indice = 0; indice < 5; indice++)
+            for(var indice = 0; indice < numeros.Length; indice++)
+            {
+                Console.WriteLine(numeros[indice]);
+            }
+        }
+    }
+}
