3a9ce38fb7a0b4178f77672cf442cb9da847ee26
Wendemberg <bergwer99@gmail.com>
2022-02-27 01:41:31 -0300

    Trabalhando classes

---
+++ Projetos/TrabalhandoClasses/TrabalhandoClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrabalhandoClasses
+{
+    class Aluno
+    {
+        public string Nome;
+        public int Idade;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aluno1 = new Aluno();
+            aluno1.Nome = "Wendemberg";
+            aluno1.Idade = 28;
+
+            Console.WriteLine(aluno1.Nome);
+            Console.WriteLine(aluno1.Idade);    
+        }
+    }
+}
