29b8d787a85d041d243594f7d5563fb30bb2b0ff
Wendemberg <bergwer99@gmail.com>
2022-02-27 01:08:58 -0300

    Estrutura Foreach

---
+++ Projetos/EstruturaRepeticaoForeach/EstruturaRepeticaoForeach/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EstruturaRepeticaoForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nomes = new[] { "Wendemberg", "Jhon", "Ph", "Felps", "Renan" };
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
