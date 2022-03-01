6adb069c3c01a44b5d88d4374397bc10c7d7e700
Wendemberg <bergwer99@gmail.com>
2022-03-01 14:48:27 -0300

    Modificadores de acesso public e rivate

---
+++ Orientacao_Objeto/ModificadoresAcessoPrivatePublic/ModificadoresAcessoPrivatePublic/Funcionario.cs
@@ -0,0 +1,9 @@
+namespace RH
+{
+    public class Funcionario
+    {
+        public string? Nome;
+
+        private string? CPF;
+    }
+}
---
+++ Orientacao_Objeto/ModificadoresAcessoPrivatePublic/ModificadoresAcessoPrivatePublic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RH;
+
+namespace ModificadoresAcessoPrivatePublic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionario = new Funcionario();
+            funcionario.Nome = "Wendemberh";
+            funcionario.CPF = "000.000.000-00";
+        }
+    }
+}
