72e23f6f64429e1fb70eb806d4a9aec7b775ce28
Wendemberg <bergwer99@gmail.com>
2022-03-01 15:10:53 -0300

    Aplicando Encapsulamento

---
+++ Orientacao_Objeto/AplicandoEncapsulamento/AplicandoEncapsulamento/Funcionario.cs
@@ -0,0 +1,28 @@
+namespace RH
+{
+    public class Funcionario
+    {
+        public string? Nome;
+
+        private string? CPF;
+        
+        public Funcionario(string nome, string cpf)
+        {
+            Nome = nome;
+            CPF = cpf;
+        }
+        public string GetCPF()
+        {
+            return CPF;
+        }
+
+        public void SetCPF(string cpf)
+        {
+            if (cpf.Length == 0)
+                return;
+            CPF = cpf;
+        }
+    }
+
+
+}
---
+++ Orientacao_Objeto/AplicandoEncapsulamento/AplicandoEncapsulamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RH;
+
+namespace AplicandoEncapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionario = new Funcionario("Wendemberg", "000.000.000-00");
+            Console.WriteLine(funcionario.GetCPF());
+            funcionario.SetCPF("111.111.111-11");
+            Console.WriteLine(funcionario.GetCPF());
+        }
+    }
+}
